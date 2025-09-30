d70586b11cc660f14eeff4381a7b49cb67ec31ae
tomcjackc <tomcjackc@gmail.com>
2025-09-30 21:17:25 +0100

    add: scripts for single predictions mod: var coefs results

---
+++ single_prediction.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name="greenlearning single prediction"
+#SBATCH --time=04:00:00
+#SBATCH --mem=8G
+#SBATCH --account=ai4er
+#SBATCH --partition=standard
+#SBATCH --qos=short
+#SBATCH -o alleq_error_vs_N_train_logs/%j.out
+#SBATCH -e alleq_error_vs_N_train_logs/%j.err
+
+for E in "third_order";
+do
+    export EQUATION=$E
+
+    for N in 75;
+    do
+    export N_TRAIN=$N
+    source /home/users/tc656/.bashrc
+
+    eval "$(conda shell.bash hook)"
+
+    conda activate greenlearning
+
+    python single_prediction.py
+    done
+done
