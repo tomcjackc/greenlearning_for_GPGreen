49d245ee070213074b47107d4570ade60009b228
tomcjackc <tomcjackc@gmail.com>
2025-10-01 00:29:48 +0100

    add: delete broken results

--- alleq_error_vs_N_train_master.sh
+++ alleq_error_vs_N_train_master.sh
@@ -19,6 +19,7 @@ eqs=(
     "nonlinear_biharmonic"
     "nonlinear_SL"
     "periodic_helmholtz"
+    "potential_barrier"
     "schrodinger"
     "third_order"
     "variable_coeffs"
@@ -35,26 +36,3 @@ do
     sbatch alleq_error_vs_N_train_single.sh
     done
 done
-
-# 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 25 30 35 40 45 50 55 60 65 70 
-# "advection_diffusion"
-#     "airy_equation"
-#     "biharmonic"
-#     "boundary_layer"
-#     "cubic_helmholtz"
-#     "cusp"
-#     "dawson"
-#     "helmholtz"
-#     "identity"
-#     "interior_layer"
-#     "jump_green"
-#     "laplace"
-#     "mean_condition"
-#     "negative_helmholtz"
-#     "nonlinear_biharmonic"
-#     "nonlinear_SL"
-#     "periodic_helmholtz"
-#     "schrodinger"
-#     "third_order"
-#     "variable_coeffs"
-#     "viscous_shock"
